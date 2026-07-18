8fef46fc9f65a8f57e3a4348eb7e67f65ab2359f
lizavetagrays <lizavetagrays@gmail.com>
2026-07-18 20:41:54 +0300

    updated method join

--- homework/elizaveta_grays/Homework_5/task_3.py
+++ homework/elizaveta_grays/Homework_5/task_3.py
@@ -4,6 +4,7 @@ last_name1, last_name2, last_name3 = students
 subjects = ['math', 'biology', 'geography']
 subject1, subject2, subject3 = subjects
 
-print(
-    'Students ' + last_name1 + ', ' + last_name2 + ', ' + last_name3 + 'study these subjects: '
-    + subject1 + ', ' + subject2 + ', ' + subject3)
+students = ', '.join(students)
+subjects = ', '.join(subjects)
+
+print('Students ' + students + 'study these subjects: ' + subjects)
