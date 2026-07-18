4b84d5739015e9629d4112e9d3cc3b5067110e3b
l_grays <lizavetagrays@gmail.com>
2026-07-18 20:52:15 +0300

    Delete homework/elizaveta_grays/Homework_6/task_2.py

--- homework/elizaveta_grays/Homework_6/task_2.py
+++
@@ -1,9 +0,0 @@
-for a in range(1, 101):
-    if a % 3 == 0 and a % 5 == 0:
-        print('FuzzBuzz')
-    elif a % 3 == 0:
-        print('Fuzz')
-    elif a % 5 == 0:
-        print('Buzz')
-    else:
-        print(a)
