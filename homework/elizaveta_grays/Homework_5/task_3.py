edbeb6439a19cb15659e067bbc548437f2beff1a
lizavetagrays <lizavetagrays@gmail.com>
2026-07-18 20:47:37 +0300

    updated method join (unsaved changes)

--- homework/elizaveta_grays/Homework_5/task_3.py
+++ homework/elizaveta_grays/Homework_5/task_3.py
@@ -1,8 +1,5 @@
 students = ['Ivanov', 'Petrov', 'Sidorov']
-last_name1, last_name2, last_name3 = students
-
 subjects = ['math', 'biology', 'geography']
-subject1, subject2, subject3 = subjects
 
 students = ', '.join(students)
 subjects = ', '.join(subjects)
