683f8fd0b5a78ee3067d4578520df8f6237582de
lizavetagrays <lizavetagrays@gmail.com>
2026-08-12 00:05:48 +0300

    homework_13 done

---
+++ homework/elizaveta_grays/Homework_13/task1.py
@@ -0,0 +1,32 @@
+import datetime
+from pathlib import Path
+
+file_path = Path.home() / 'PycharmProjects/Elizabeth/homework/eugene_okulik/hw_13/data.txt'
+
+
+def read_file():
+    with open(file_path, 'r') as data_file:
+        for line in data_file.readlines():
+            yield line
+
+
+def process_line(line):
+    parts = line.split(' - ')
+    date_str = parts[0].split('. ')[1]
+    data_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S.%f')
+    text = parts[1]
+    if not line.strip():
+        return
+    if 'на неделю позже' in text:
+        new_date = data_obj + datetime.timedelta(days=7)
+        print(new_date)
+    elif 'день недели' in text:
+        week = data_obj.strftime('%A')
+        print(week)
+    elif 'дней назад' in text:
+        now = datetime.datetime.now()
+        date_ago = now - data_obj
+        print(date_ago.days)
+
+
+for line in read_file(): process_line(line)
