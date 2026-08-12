0dc48362325e4a1b0ff66d4d5bea6ccd51cd61a2
lizavetagrays <lizavetagrays@gmail.com>
2026-08-12 14:38:28 +0300

    fixed file path retrieval and moved empty line check

--- homework/elizaveta_grays/Homework_13/task1.py
+++ homework/elizaveta_grays/Homework_13/task1.py
@@ -1,22 +1,24 @@
 import datetime
-from pathlib import Path
+import os
 
-file_path = Path.home() / 'PycharmProjects/Elizabeth/homework/eugene_okulik/hw_13/data.txt'
+base_path = os.path.dirname(__file__)
+homework_path = os.path.dirname(os.path.dirname(base_path))
+eugene_file_path = os.path.join(homework_path, 'eugene_okulik', 'hw_13', 'data.txt')
 
 
 def read_file():
-    with open(file_path, 'r') as data_file:
+    with open(eugene_file_path, 'r') as data_file:
         for line in data_file.readlines():
             yield line
 
 
 def process_line(line):
+    if not line.strip():
+        return
     parts = line.split(' - ')
     date_str = parts[0].split('. ')[1]
     data_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S.%f')
     text = parts[1]
-    if not line.strip():
-        return
     if 'на неделю позже' in text:
         new_date = data_obj + datetime.timedelta(days=7)
         print(new_date)
