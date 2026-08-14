3c819e5f5cd4bcf17fb392a4ee3e94083c26223f
lizavetagrays <lizavetagrays@gmail.com>
2026-08-14 15:51:50 +0300

    added loops, fixed comments

--- homework/elizaveta_grays/Homework_15/db.py
+++ homework/elizaveta_grays/Homework_15/db.py
@@ -14,15 +14,15 @@ query = 'INSERT INTO students (name, second_name) VALUES (%s, %s)'
 values = ('Maria', 'Ivanova')
 cursor.execute(query, values)
 student_id = cursor.lastrowid
-cursor.execute(f'SELECT * FROM students WHERE id = {student_id}')
+cursor.execute('SELECT id FROM students WHERE id = %s', (student_id,))
 print(cursor.fetchone())
 db.commit()
 
 query = 'INSERT INTO books (title, taken_by_student_id) VALUES (%s, %s)'
 values = [
-    ('Harry Potter', 23078),
-    ('The Lord of the Rings', 23078),
-    ('Sherlock Holmes', 23078)
+    ('Harry Potter', student_id),
+    ('The Lord of the Rings', student_id),
+    ('Sherlock Holmes', student_id)
 ]
 cursor.executemany(query, values)
 db.commit()
@@ -31,12 +31,12 @@ query = 'INSERT INTO `groups` (title, start_date, end_date) VALUES (%s, %s, %s)'
 values = ('High Group', '13 jul 2026', '31 dec 2026')
 cursor.execute(query, values)
 group_id = cursor.lastrowid
-cursor.execute(f'SELECT * FROM `groups` WHERE id = {group_id}')
+cursor.execute('SELECT id FROM `groups` WHERE id = %s', (group_id,))
 print(cursor.fetchone())
 db.commit()
 
 query = 'UPDATE students SET group_id = %s where id = %s'
-values = (23079, 23078)
+values = (group_id, student_id)
 cursor.execute(query, values)
 db.commit()
 
@@ -45,46 +45,52 @@ values = [('History',),
           ('Mathematic',),
           ('Literature',)
           ]
-cursor.executemany(query, values)
-cursor.execute(
-    "SELECT * FROM subjects WHERE title IN (%s, %s, %s) ORDER BY id DESC LIMIT 3",
-    ('History', 'Mathematic', 'Literature'))
-print(cursor.fetchall())
+subject_ids = {}
+for title in values:
+    subject_name = title[0]
+    cursor.execute(query, (subject_name,))
+    subject_id = cursor.lastrowid
+    subject_ids[subject_name] = subject_id
+print(subject_ids)
 db.commit()
 
 query = 'INSERT INTO lessons (title,subject_id) VALUES(%s,%s)'
 values = [
-    ('Literature lesson 1', 23121),
-    ('Literature lesson 2', 23121),
-    ('Mathematic lesson 1', 23120),
-    ('Mathematic lesson 2', 23120),
-    ('History lesson 1', 23119),
-    ('History lesson 2', 23119)
+    ('Literature lesson 1', 'Literature'),
+    ('Literature lesson 2', 'Literature'),
+    ('Mathematic lesson 1', 'Mathematic'),
+    ('Mathematic lesson 2', 'Mathematic'),
+    ('History lesson 1', 'History'),
+    ('History lesson 2', 'History')
 ]
-cursor.executemany(query, values)
-cursor.execute(
-    "SELECT id, title FROM lessons WHERE title IN (%s, %s, %s,%s, %s, %s) ORDER BY id DESC LIMIT 6",
-    ('Literature lesson 1', 'Literature lesson 2', 'Mathematic lesson 1', 'Mathematic lesson 2',
-     'History lesson 1', 'History lesson 2'))
-print(cursor.fetchall())
+lessons_to_insert = {}
+for item in values:
+    lesson_name = item[0]
+    subject_name = item[1]
+    subject_id = subject_ids[subject_name]
+    cursor.execute(query, (lesson_name, subject_id,))
+    lesson_id = cursor.lastrowid
+    lessons_to_insert[lesson_name] = lesson_id
+print(lessons_to_insert)
+
 db.commit()
 
 query = 'INSERT INTO marks (value,lesson_id, student_id) VALUES (%s, %s, %s)'
 values = [
-    (5, 76452, 23078),
-    (3, 76453, 23078),
-    (4, 76451, 23078),
-    (3, 76450, 23078),
-    (5, 76449, 23078),
-    (5, 76448, 23078)
+    (5, lessons_to_insert['Literature lesson 1'], student_id),
+    (3, lessons_to_insert['Literature lesson 2'], student_id),
+    (4, lessons_to_insert['Mathematic lesson 1'], student_id),
+    (3, lessons_to_insert['Mathematic lesson 2'], student_id),
+    (5, lessons_to_insert['History lesson 1'], student_id),
+    (5, lessons_to_insert['History lesson 2'], student_id)
 ]
 cursor.executemany(query, values)
 db.commit()
 
-cursor.execute('SELECT value FROM marks where student_id = %s', (23078,))
+cursor.execute('SELECT value FROM marks where student_id = %s', (student_id,))
 print(cursor.fetchall())
 
-cursor.execute('SELECT title from books where taken_by_student_id = %s', (23078,))
+cursor.execute('SELECT title from books where taken_by_student_id = %s', (student_id,))
 print(cursor.fetchall())
 
 cursor = db.cursor(dictionary=True)
@@ -95,9 +101,10 @@ query = ''' SELECT g.id, g.title, b.title, m.value , l.title , s2.title
     join `groups` g on g.id = s.group_id
     join lessons l on l.id = m.lesson_id
     join subjects s2 on s2.id = l.subject_id
-    where s.id = 23078
+    where s.id = %s
     '''
-cursor.execute(query)
+cursor.execute(query, (student_id,))
 print(cursor.fetchall())
 
+cursor.close()
 db.close()
