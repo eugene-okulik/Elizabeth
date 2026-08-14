19440171a81334e68ee135bc0b7df105bb94279f
lizavetagrays <lizavetagrays@gmail.com>
2026-08-14 13:03:05 +0300

    fixed all comments

--- homework/elizaveta_grays/Homework_14/scripts.sql
+++ homework/elizaveta_grays/Homework_14/scripts.sql
@@ -5,9 +5,9 @@ INSERT INTO books (title,taken_by_student_id) VALUES
 ('War and World', 23076),
 ('Evgenii Onegin', 23076)
 
-INSERT INTO `groups` (id,title,start_date,end_date) VALUES (23076,'Tester', '01 aug 2026', '31 aug 2026')
+INSERT INTO `groups` (title,start_date,end_date) VALUES ('Tester', '01 aug 2026', '31 aug 2026')
 
-UPDATE students SET groups_id=23076 WHERE id=23076
+UPDATE students SET group_id=23076 WHERE id=23076
 
 INSERT INTO subjects (title) VALUES ('geography'), ('english'),('biology')
 
@@ -40,7 +40,7 @@ SELECT g.id, g.title, b.title, m.value , l.title , s2.title
 from students s
 join books b on s.id = b.taken_by_student_id
 join marks m on s.id = m.student_id
-join `groups` g on g.id = s.id
+join `groups` g on s.group_id = g.id
 join lessons l on l.id = m.lesson_id
 join subjects s2 on s2.id = l.subject_id
 where s.id = 23076
